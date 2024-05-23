553f5dbbbaf10c54ed3a0a3f4275c4057243fa95
ph-e <122577043+ph-e@users.noreply.github.com>
2024-05-23 22:37:46 +0300

    Update workcloudinary.py
    add more info parse(seller,delivery,stock and more)

--- workcloudinary.py
+++ workcloudinary.py
@@ -5,9 +5,10 @@ import cloudinary.uploader
 import os
 import random
 import string
+from bs4 import BeautifulSoup
 
 def parse(product_url):
-    # We receive the title and links to product photos on ebay
+    #We receive the title and links to product photos on ebay
     dataarray = []
     response = requests.get(product_url)
     if response.status_code == 200:
@@ -19,11 +20,57 @@ def parse(product_url):
             captured_text = pText.group(1)
             title = re.sub(r'.*?"title":"', '', captured_text)
             dataarray.append('For ' + title)
+        soup = BeautifulSoup(text, 'html.parser')
+
+
+
+        
+        # Extracting price
+        price_div = soup.find('div', class_='x-price-primary')
+        if price_div:
+            price_span = price_div.find('span', class_='ux-textspans')
+    
+            if price_span:
+                dataarray.append('Price: ' + price_span.text)
+
+
+
+        # Extracting shipping price
+        shipping_div = soup.find('div', class_='ux-labels-values__values-content')
+        if shipping_div:
+            shipping = shipping_div.find('span', class_='ux-textspans ux-textspans--BOLD')
+            if shipping:
+                dataarray.append(shipping.text)
+
+        # Extracting stock
+        stock_div = soup.find('div', class_='d-quantity__availability')
+        if stock_div:
+            stock_span = stock_div.find('span', class_='ux-textspans')
+            if stock_span:
+                dataarray.append(stock_span.text)
+
+        # Extrecting Delivery
+        delivery_div = soup.find_all('div', class_='ux-labels-values__values-content')
+        if len(delivery_div) >= 2:
+            delivery_values_div = delivery_div[1]
+            spans = delivery_values_div.find_all('span', class_='ux-textspans')
+            delivery_value = ' '.join(span.text for span in spans[:4])
+            dataarray.append(delivery_value)
+
+        # Extrecting Seller
+        seller_info_div = soup.find('div', class_='x-sellercard-atf__info__about-seller')
+        if seller_info_div:
+            seller_span = seller_info_div.find('span', class_='ux-textspans ux-textspans--BOLD')
+            if seller_span:
+                dataarray.append(seller_span.text)
+
+
+
 
         pattern = r'"(https://i\.ebayimg\.com/images/[^"]*/s-l1600\.(jpg|png))"'
         matches = re.findall(pattern, text)
         for match in matches:
-            if match[0] not in dataarray:  # Uniqueness check
+            if match[0] not in dataarray:  # Проверка на уникальность
                 dataarray.append(match[0])
 
         print('Information copied successfully')
@@ -33,7 +80,7 @@ def parse(product_url):
     return dataarray
 
 def upload_image_to_cloudinary(api_key, api_secret, cloud_name, image_path):
-    # Uploading downloaded photos and returning a link to them
+    #uploading downloaded photos and returning a link to them
     try:
         cloudinary.config(
             cloud_name=cloud_name,
@@ -55,7 +102,7 @@ def upload_image_to_cloudinary(api_key, api_secret, cloud_name, image_path):
     return ""
 
 def download_image(image_url, save_path):
-    # Download photos from the link
+    #download photos from the link
     try:
         response = requests.get(image_url)
         if response.status_code == 200:
@@ -68,30 +115,30 @@ def download_image(image_url, save_path):
         print("An error occurred while downloading the image:", str(e))
 
 def getLinks(urlSeller):
-    # We receive links to all products located on the page
+    #We receive links to all products located on the page
     response = requests.get(urlSeller)
     if response.status_code == 200:
         text = response.text
         pattern = r'https://www\.ebay\.com/itm/[^"\s]+'
         links = re.findall(pattern, text)
-        # Remove the line from each link
+        # Удаляем строку "> <div" из каждой ссылки
         links = [re.sub(r'><div', '', link) for link in links]
         return links
     return ''
 
 def main():
 
-    # We get a link to the product page
+    #We get a link to the product page
     urlSeller = input("Enter the link to the seller's page:")
 
-    # Count of images
+    #Count of images
     countIMG = int(input("Enter the maximum number of images to upload:"))
 
-    # Open a text document for reading
+    #Open a text document for reading
     with open("config.txt", "r") as file:
         lines = file.readlines()
 
-    # Loop through strings and look for key values
+    #Loop through strings and look for key values
     api_key = None
     api_secret = None
     cloud_name = None
@@ -104,27 +151,35 @@ def main():
         elif "cloud_name" in line:
             cloud_name = line.split("=")[1].strip()
 
-    # Creating a new excel table
+    #creating a new excel table
     workbook = openpyxl.Workbook()
     worksheet = workbook.active
 
-    # Receiving a link to the seller's items
+    #Receiving a link to the seller's items
     links = getLinks(urlSeller)
     info = []
     processed_links = set()
     for idx, link in enumerate(links):
-        if link not in processed_links: # Checking for duplicate links
+        if link not in processed_links: # checking for duplicate links
             if link != "":
                 processed_links.add(link)
                 info = parse(link)
                 indexRow = len(processed_links)
                 worksheet.cell(row=indexRow, column=1, value=link)
                 for index, item in enumerate(info):
-                    if index > countIMG:  # Upload no more than count photos
-                        break
                     if index == 0:
                         worksheet.cell(row=indexRow, column=2, value=info[0])
-                    else:
+                    elif index == 1:
+                        worksheet.cell(row=indexRow, column=3, value=item)
+                    elif index == 2:
+                        worksheet.cell(row=indexRow, column=4, value=item)
+                    elif index == 3:
+                        worksheet.cell(row=indexRow, column=5, value=item)
+                    elif index == 4:
+                        worksheet.cell(row=indexRow, column=6, value=item)
+                    elif index == 5:
+                        worksheet.cell(row=indexRow, column=7, value=item)
+                    elif index <= (countIMG + 5):
                         image_url = item
                         image_path = f"downloaded_images/{indexRow}.jpg"
                         try:
@@ -136,7 +191,7 @@ def main():
                         except Exception as e:
                             print(f"ERROR Failed to download image: {e}")
 
-    # Generating a random file name
+    #Generating a random file name
     file_name = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))                    
     # Save the modified workbook
     workbook.save(file_name + ".xlsx")
